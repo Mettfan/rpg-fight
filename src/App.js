e32bee9aa9c514f01e5c50269091297f2f4b4be2
Mettfan <cesaryannick6@gmail.com>
2022-10-09 10:41:21 -0500

    platilla de ataque

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let user = {
+    name: '',
+    entry: '',
+    life: 100,
+    attacks: [
+      {
+        name: 'kick',
+        damage: 10,
+      },
+      {
+        name: 'punch',
+        damage: 5,
+      },
+  ],
+    shield: Math.random()*10, //Could be any number
+    lives: 3,
+    potions: [{
+      evade: {
+        damage: 0,
+        health: 0
+      },
+      torsHammer: {
+        damage: 30,
+        health: 0
+      },
+      health: {
+        damage: 0,
+        health: 20
+      }
+    }]
+
+  }
+  let enemy = {
+    name: '',
+    entry: '',
+    life: 100,
+    attacks: [
+      {
+        name: 'kick',
+        damage: 10,
+      },
+      {
+        name: 'punch',
+        damage: 5,
+      },
+  ],
+    shield: Math.random()*10, //Could be any number
+    lives: 3,
+    potions: [{}]
+
+  }
+  let [state, setState] = useState({
+    user,
+    enemy
+  })
+  let turn = (player, attack, objective) => {
+    console.log(player, attack, objective);
+    let move = state[player]?.attacks?.find( move => {
+      return move.name == attack
+    })
+    setState({
+      ...state,
+      [objective]: {
+        ...state[objective],
+        life: state[objective]?.life - (move?.damage || 0)
+      }
+    })
+    // objective.life -= player[attack]
+
+  }
+  let handleUserChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: {
+        ...state[e.target.name],
+        entry: e.target.value
+      }
+    })
+    console.log(e.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='userEntry'>
+        <div>Vida: { ' ' + state.user.life }</div>
+        <input name='user' onChange={(e) => {handleUserChange(e)}} type={'text'} placeholder={'Ingrese su ataque'}></input>
+        <button onClick={() => {turn('user', state.user.entry, 'enemy')}}>Ataque</button>     
+        {JSON.stringify(state.user)}      
+      </div>
+
+      <div className='enemyEntry'>
+      <div>Vida: { ' ' + state.enemy.life }</div>
+        <input name='enemy' onChange={(e) => {handleUserChange(e)}} type={'text'} placeholder={'Ingrese su ataque'}></input>           
+        <button onClick={ () => {turn('enemy', state.enemy.entry, 'user')} }>Ataque</button>           
+        {JSON.stringify(state.enemy)}      
+      </div>
+
+      
+
     </div>
   );
 }
